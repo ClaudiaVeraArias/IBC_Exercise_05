25213fafa14e5b15244c8479373f45d1a7b44060
Tom Moran <thomasemoran91@gmail.com>
2019-10-09 18:53:15 -0400

    update for highest and lowest earners

--- collab2.sh
+++ collab2.sh
@@ -1,1 +1,5 @@
-cat wages.csv | sort -n -t , -k 4 | tail -n 1
+highwage=$(cat wages.csv | sort -n -t , -k 4 | tail -n 1 | cut -d , -f 1,2,4)
+lowwage=$(cat wages.csv | sort -n -t , -k 4 | head -n 2 | grep male | cut -d , -f 1,2,4)
+echo $highwage
+echo $lowwage
+
