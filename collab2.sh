27a1745a9e3e6d91bf78532597be89129f1eb482
Tom Moran <thomasemoran91@gmail.com>
2019-10-09 19:01:54 -0400

    completed step 2

--- collab2.sh
+++ collab2.sh
@@ -1,5 +1,6 @@
 highwage=$(cat wages.csv | sort -n -t , -k 4 | tail -n 1 | cut -d , -f 1,2,4)
 lowwage=$(cat wages.csv | sort -n -t , -k 4 | head -n 2 | grep male | cut -d , -f 1,2,4)
-echo $highwage
-echo $lowwage
-
+echo "Highest wage earner: $highwage"
+echo "Lowest wage earner: $lowwage"
+TopFem=$(cat wages.csv | sort -n -t , -k 4 | tail -n 10 | grep 'female' | wc -l)
+echo "The number of women in the top ten wage earners is $TopFem"
