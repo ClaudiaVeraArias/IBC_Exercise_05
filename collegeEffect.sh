afff5269038550d6ba88d1ce2ccc48da3a759785
Tom Moran <thomasemoran91@gmail.com>
2019-10-09 19:50:14 -0400

    got part of it done

--- collegeEffect.sh
+++ collegeEffect.sh
@@ -1,3 +1,5 @@
 collegegrad=$(cat wages.csv | grep -e '[0-9],16,' | cut -d , -f 4)
-HSgrad=$(cat wages.csv | grep -e '[0-9],12,' | cut -d , -f 4 | awk '{total += $0} END {print total}')
-echo $HSgrad	 
+HSsum=$(cat wages.csv | grep -e '[0-9],12,' | cut -d , -f 4 | awk '{total += $0} END {print total}')
+HSnum=$(cat wages.csv | grep -e '[0-9],12,' | wc -l)
+echo "$HSsum / $HSnum" | bc
+	 
