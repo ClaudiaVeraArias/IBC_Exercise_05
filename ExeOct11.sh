a2cf6c2083c139cc12e2eb0adc3792370fa1b400
ClaudiaVeraArias <55985345+ClaudiaVeraArias@users.noreply.github.com>
2019-10-09 18:19:04 -0400

    Update ExeOct11.sh

--- ExeOct11.sh
+++ ExeOct11.sh
@@ -7,4 +7,4 @@ cat wages.csv | cut -d , -f 1,2 >> ExGib.csv
 cat ExGib.csv |  sed 's/,/ /g' >> ExGib2.txt
 egrep -w 'male' ExGib2.txt | sort -u -n -k 2 >> ExGib3.txt 
 egrep -w 'female' ExGib2.txt | sort -u -n -k 2 >> ExGib3.txt
-
+cat ExGib3.txt
