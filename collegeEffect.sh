60624cb385197af3c8452607ddd108429aefbd1d
ClaudiaVeraArias <55985345+ClaudiaVeraArias@users.noreply.github.com>
2019-10-09 21:56:45 -0400

    I think we are more close

--- collegeEffect.sh
+++ collegeEffect.sh
@@ -2,11 +2,13 @@ collegegrad=$(cat wages.csv | grep -e '[0-9],16,' | cut -d , -f 4)
 
 HSsum=$(cat wages.csv | grep -e '[0-9],12,' | cut -d , -f 4 | awk '{total += $0} END {print total}')
 HSnum=$(cat wages.csv | grep -e '[0-9],12,' | wc -l)
-echo "$HSsum / $HSnum" | bc > 12Years.txt
+echo "$HSsum / $HSnum" | bc 
 	 
 collegesum=$(cat wages.csv | grep -e '[0-9],16,' | cut -d , -f 4 | awk '{total += $0} END {print total}')
 collegenum=$(cat wages.csv | grep -e '[0-9],16,' | wc -l)
-echo "$collegesum / $collegenum" | bc > 16Years.txt
+echo "$collegesum / $collegenum" | bc
 
-echo "16Years - 12Years" | bc
-
+x=$(echo "$HSsum / $HSnum" | bc)
+y=$(echo "$collegesum / $collegenum" | bc)
+final=$((y-x))
+echo $final
