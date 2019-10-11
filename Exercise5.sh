8cfda8dff7677258c372347369ac24b036257128
Tom Moran <thomasemoran91@gmail.com>
2019-10-11 10:08:54 -0400

    Changes to what is echoed

--- Exercise5.sh
+++ Exercise5.sh
@@ -1,33 +1,46 @@
-echo 1
+echo Part 1
 
+# Write unique gender-yearsExperience combinations in ExGib3.txt
 cat wages.csv | cut -d , -f 1,2 >> ExGib.csv
 cat ExGib.csv |  sed 's/,/ /g' >> ExGib2.txt
 egrep -w 'male' ExGib2.txt | sort -u -n -k 2 >> ExGib3.txt 
 egrep -w 'female' ExGib2.txt | sort -u -n -k 2 >> ExGib3.txt
 cat ExGib3.txt
 
-echo 2
+echo Part 2
+# Report to standard out Highest and Lowest wager earners and the number of 
+# female wage earners in the top ten. 
 
 highwage=$(cat wages.csv | sort -n -t , -k 4 | tail -n 1 | cut -d , -f 1,2,4)
 lowwage=$(cat wages.csv | sort -n -t , -k 4 | head -n 2 | grep male | cut -d , -f 1,2,4)
 echo "Highest wage earner: $highwage"
 echo "Lowest wage earner: $lowwage"
 TopFem=$(cat wages.csv | sort -n -t , -k 4 | tail -n 10 | grep 'female' | wc -l)
-echo "The number of women in the top ten wage earners is $TopFem"
+echo "The number of women in the top ten wage earners is $TopFem."
 
-echo 3
+echo Part 3
 
-collegegrad=$(cat wages.csv | grep -e '[0-9],16,' | cut -d , -f 4)
+# Calculate the average wages of those with 12 years
+# of education experience, i.e. HS degree earner.
 
 HSsum=$(cat wages.csv | grep -e '[0-9],12,' | cut -d , -f 4 | awk '{total += $0} END {print total}')
 HSnum=$(cat wages.csv | grep -e '[0-9],12,' | wc -l)
-echo "$HSsum / $HSnum" | bc 
-	 
+x=$(echo "$HSsum / $HSnum" | bc)
+
+# Calculate the average wage of those with 16 years
+# of education experience, i.e. college degree earner.
+
 collegesum=$(cat wages.csv | grep -e '[0-9],16,' | cut -d , -f 4 | awk '{total += $0} END {print total}')
 collegenum=$(cat wages.csv | grep -e '[0-9],16,' | wc -l)
-echo "$collegesum / $collegenum" | bc
-
-x=$(echo "$HSsum / $HSnum" | bc)
 y=$(echo "$collegesum / $collegenum" | bc)
+
+# Calculate the difference in average wages between
+# a HS degree earner and a college degree earner.
+
 final=$((y-x))
-echo $final
+
+# Report the calculations
+
+echo "Wage earnings of the average HS graduate: $x dollars"
+echo "Wage earnings of the average college graduate : $y dollars"
+echo "A college graduate earns on average $final dollars more than those that merely completed High School."
